e33d712629b94f86be39d817fe5049a3b30155b0
tominin.vd <v.tominin@ira-labs.com>
2022-12-04 15:30:26 +0300

    camembert: new training parameters

--- retweet_prediction_challenge/scripts/camembert_fine_tuning.py
+++ retweet_prediction_challenge/scripts/camembert_fine_tuning.py
@@ -6,9 +6,9 @@ from retweet_prediction_challenge.code.model import CamembertRegressor
 
 dataset = get_custom_dataset()
 
-training_args = dict(output_dir="/shared/personal/vladtom/exp2",
+training_args = dict(output_dir="/shared/personal/vladtom/exp3_lr1e5",
                      label_names=['labels'],
-                     learning_rate=1e-4,
+                     learning_rate=1e-5,
                      save_total_limit=20,
                      fp16=True,
                      per_device_train_batch_size=38,
@@ -16,7 +16,7 @@ training_args = dict(output_dir="/shared/personal/vladtom/exp2",
                      num_train_epochs=2,
                      logging_strategy='steps',
                      evaluation_strategy='steps',
-                     eval_steps=10000,
+                     eval_steps=1000,
                      )
 
 model = CamembertRegressor(training_args)
